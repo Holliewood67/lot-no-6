357a214a5137b3f281b2cf794e6e8540fa32c449
Holliewood67 <ethancantrell67@gmail.com>
2025-06-18 07:36:16 -0500

    fixed typos in hero text

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -2,8 +2,8 @@ export default function Hero() {
     return(
         <section className="h-screen w-full flex flex-col items-center justify-center text-center px-4 py-12 border-b-2">
             <h1 className="text-3xl md:text-4xl">Welcome to Lot No. 6</h1>
-            <p className="text-lg md:text 2xl md:max-w-xl lg:max-w-3xl py-6">Tulsa`&apos;`s favorite stop for art, karaoke, live music, ^ more, located in the heart of the Pearl District</p>
-            <a href="/events" className="py-2 px-4 bg-gray-600/25 rounded-lg">See What`&apos;`s Happening</a>
+            <p className="text-lg md:text 2xl md:max-w-xl lg:max-w-3xl py-6">Tulsa&apos;s favorite stop for art, karaoke, live music, & more, located in the heart of the Pearl District</p>
+            <a href="/events" className="py-2 px-4 bg-gray-600/25 rounded-lg">See What&apos;s Happening</a>
         </section>
     )
 }
