332922fcec38060f0f04bc2d9154a5a5842212e0
Holliewood67 <ethancantrell67@gmail.com>
2025-06-21 12:01:42 -0500

    changed mr test bio

--- src/app/data/artist-list.ts
+++ src/app/data/artist-list.ts
@@ -9,7 +9,7 @@ const artistList: Artist[] = [
   {
     name: "Mr. Test",
     imageUrl: "/artist-pics/test.png",
-    bio: "Mr. Test is a Tulsa-based folk artist whose introspective lyrics and stripped-down style make him a standout performer. Catch him live at Lot No. 6 this month!",
+    bio: "Mr. Test is a a Tulsa-based artist who doesn't actually exist, but is rather a placeholder just to show how a real bio would look here along with some kind of image. What do you think?",
     links: {
       instagram: "https://instagram.com",
       spotify: "https://spotify.com",
