0ca031722bcac4f840992fcd89154d9fdf29f1dd
Holliewood67 <ethancantrell67@gmail.com>
2025-06-25 03:48:54 -0500

    added opengraph info the the layout file for social media sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,29 @@ import { Limelight } from 'next/font/google'
 })
 
 
-export const metadata: Metadata = {
-  title: "Lot No. 6 - Tulsa's #1 Art and Karaoke Bar",
-  description: "Tulsaa's favorite art & karaoke bar",
-};
+export const metadata = {
+  title: "Lot No. 6 - Tulsa's #1 Art & Karaoke Bar",
+  description: "Tulsa’s hub for art, karaoke, and live music—Lot No. 6 is a cozy, creative bar in the Pearl District where open mics, monthly art shows, and community vibes come together every week.",
+  siteName: 'Lot No. 6',
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  openGraph: {
+    title: "Lot No. 6 - Tulsa's #1 Art & Karaoke Bar",
+    description: "Tulsa’s hub for art, karaoke, and live music—Lot No. 6 is a cozy, creative bar in the Pearl District where open mics, monthly art shows, and community vibes come together every week.",
+  
+    images: [
+      {
+        url: "https://master.d2p7nbas23b057.amplifyapp.com/lot-6.jpg", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ]
+  },
+}
 
 export default function RootLayout({
   children,
